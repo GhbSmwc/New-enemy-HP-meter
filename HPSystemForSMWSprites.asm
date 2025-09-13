85d34d4a99f621e5b568699a5cdc7803fa5352b5
? <GhbSmwc@users.noreply.github.com>
2025-09-13 18:20:48 -0400

    WIP
    moving subroutines to shared subroutines is mostly complete. Gotta remove redundant subroutines in uberasm tool and pixi

--- HPSystemForSMWSprites.asm
+++ HPSystemForSMWSprites.asm
@@ -22,7 +22,7 @@ incsrc "Defines/GraphicalBarDefines.asm"
 					LDA #$00                                                ;>...Set it to 0.
 				?.NotMoreThanEnoughDamage
 					STA !Freeram_SpriteHP_CurrentHPLow,x                    ;>otherwise just write the non-negative difference as HP.
-				if !Setting_SpriteHP_TwoByte != 0
+				if !Setting_SpriteHP_TwoByte
 					LDA #$00                                                ;\Rid high bytes.
 					STA !Freeram_SpriteHP_CurrentHPHi,x                     ;|(So far, there is never a sprite that stores a 16-bit damage counter)
 					STA !Freeram_SpriteHP_MaxHPHi,x                         ;/
@@ -33,7 +33,16 @@ incsrc "Defines/GraphicalBarDefines.asm"
 	macro IncreaseDamageCounter(DamageCountSpriteTableRAM, DamageAmount, DamageAmountToDie)
 		?Damage:
 		if !Setting_SpriteHP_NoDisplaySMWSpriteHP == 0
-			JSL SwitchHPDisplay
+			if !Setting_SpriteHP_TwoByte
+				REP #$20
+				LDA.w #<DamageAmount>
+				STA $00
+				SEP #$20
+			else
+				LDA.b #<DamageAmount>
+				STA $00
+			endif
+			JSL !SharedSub_SpriteHPDamage
 		endif
 		LDA <DamageCountSpriteTableRAM>,x
 		CLC
@@ -292,7 +301,7 @@ incsrc "Defines/GraphicalBarDefines.asm"
 ;Freespace code
 	freecode
 	if and(!Setting_SpriteHP_ModifySMWSprites, !Setting_SpriteHP_VanillaSprite_Chuck)
-		CharginChuckHitCountToHP:	;>JML from $02C1F8
+		CharginChuckHitCountToHP:	;>JML from $02C1F8 (runs every frame)
 			LDA !14C8,x
 			CMP #$02
 			BCC .Restore		;>Do nothing if $00~$01
@@ -309,26 +318,11 @@ incsrc "Defines/GraphicalBarDefines.asm"
 				%ConvertDamageAmountToHP(!1528, !Setting_SpriteHP_VanillaSprite_ChuckHPAmount)
 				
 				if !Setting_SpriteHP_BarAnimation
-					JSL GetHPMeterSlotIndexNumber
+					JSL !SharedSub_SpriteHPGetSlotIndex
 					TXA
 					CMP !Scratchram_SpriteHP_SpriteSlotToDisplay
 					BEQ ..OnCurrentSprite
-					if !Setting_SpriteHP_BarFillRoundDirection == 0
-						JSL RemoveRecord
-					elseif !Setting_SpriteHP_BarFillRoundDirection == 1
-						JSL RemoveRecordRoundDown
-					elseif !Setting_SpriteHP_BarFillRoundDirection == 2
-						JSL RemoveRecordRoundUP
-					endif
-					if !Setting_SpriteHP_GraphicalBar_RoundAwayEmptyFull == 1
-						JSL RoundAwayEmpty
-					elseif !Setting_SpriteHP_GraphicalBar_RoundAwayEmptyFull == 2
-						JSL RoundAwayFull
-					elseif !Setting_SpriteHP_GraphicalBar_RoundAwayEmptyFull == 3
-						JSL RoundAwayEmptyFull
-					endif
-					LDA $00
-					STA !Freeram_SpriteHP_BarAnimationFill,x
+					JSL !SharedSub_SpriteHPRemoveRecordEffect
 					..OnCurrentSprite
 				endif
 			
@@ -450,304 +444,3 @@ incsrc "Defines/GraphicalBarDefines.asm"
 				STA $1DFC|!addr
 				RTL
 	endif
-;Various subroutines below
-	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-	;This essentially takes the value of !Freeram_SpriteHP_MeterState, and modulo by
-	;!sprite_slots, which can be used to detect if what sprite slot index number the meter is
-	;on is on the same slot number as the currently processed sprite.
-	;
-	;Output:
-	; - !Scratchram_SpriteHP_SpriteSlotToDisplay: Sprite slot index number
-	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-	GetHPMeterSlotIndexNumber:
-		LDA #$FF
-		CMP !Freeram_SpriteHP_MeterState
-		BEQ .NonIntroFillMode
-		LDA !Freeram_SpriteHP_MeterState
-		CMP.b #!sprite_slots
-		BCC .NonIntroFillMode
-		.IntroFillMode
-			SEC
-			SBC.b #!sprite_slots
-		.NonIntroFillMode
-		STA !Scratchram_SpriteHP_SpriteSlotToDisplay
-		RTL
-		
-	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-	;Switch HP display
-	;Input:
-	; - X: current sprite slot
-	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-	if !Setting_SpriteHP_NoDisplaySMWSpriteHP == 0
-		SwitchHPDisplay:
-			LDA !Freeram_SpriteHP_MeterState
-			CMP #$FE
-			BEQ .Locked
-			CMP #$FD
-			BEQ .Locked
-			TXA
-			STA !Freeram_SpriteHP_MeterState
-			if and(!Setting_SpriteHP_BarAnimation, notequal(!Setting_SpriteHP_BarChangeDelay, 0))
-				LDA.b #!Setting_SpriteHP_BarChangeDelay
-				STA !Freeram_SpriteHP_BarAnimationTimer,x
-			endif
-			.Locked
-			RTL
-	endif
-	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-	;Remove record effect, lesser-RAM-based and shorter edition
-	;
-	;Unlike the provicded custom sprite, which calls a subroutine
-	;("%SpriteHP_RemoveRecordEffect()") to figure out the amount of fill in the bar of its
-	;current HP for $00 to set !Freeram_SpriteHP_BarAnimationFill, this uses precalculated
-	;total pieces in the bar to reduce code duplicates in both this patch and pixi routines.
-	;These aformentioned subroutines have lots of dependencies with other grapical bar
-	;routines and other math routines.
-	;
-	;This is at the cost of certain customizability, such as variable-length bar that allows
-	;dynamic calculations with variable bar in-game.
-	;
-	;Input:
-	; - X = Current sprite slot
-	; - !Freeram_SpriteHP_MaxHPLow = sprite current HP (for display)
-	;Output:
-	; - $00~$01: Amount of fill in the bar at the sprite's current HP
-	; - Y: $00 if not rounded to empty or full, $01 if rounded to empty, $02 if rounded to
-	;   full.
-	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-	if !Setting_SpriteHP_BarFillRoundDirection == 0
-		RemoveRecord:
-			LDA !Freeram_SpriteHP_MaxHPLow,x
-			STA !Scratchram_GraphicalBar_FillByteTbl+2
-			LDA #$00
-			STA !Scratchram_GraphicalBar_FillByteTbl+3
-			JSL RemoveRecordRoundDown
-				; - $00~$03: Amount of fill in the bar (rounded down)
-				; - $04~$05: Remainder ranging from 0 to MaxHeath-1 to be used for rounding
-				; - Y: Round to zero flag: $00 = no $01 = yes.
-			REP #$20
-			LDA !Scratchram_GraphicalBar_FillByteTbl
-			LSR
-			BCC .ExactHalf
-				;^Reason 1/2 point must be rounded up, is so to truly check if remainder is greater or equal to half of MaxQuantity, to round the pieces filled up,
-				; when MaxQuantity is odd. e.g. 1 Quantity * 62 pieces / 5 MaxQuantity = Q:12 R:2, which is 12 and 2/5, or 12.4. The 1/2 point of 5 is EXACTLY 2.5,
-				; not 2 (LSR divides A by 2 and round down, thus resulting 2). This means that the lowest remainder integer to trigger a round-up of the amount of
-				; pieces filled would be 3.
-			INC
-			.ExactHalf
-				CMP $04						;>Half of denominator compares with remainder
-				BEQ .RoundQuotient				;>Half equal remainder, round up
-				BCS .NoRoundQuotient				;>Half greater than remainder (remainder being smaller), keep the already-floored quotient as is
-				
-				.RoundQuotient
-				LDA $00						;\Round up an integer
-				INC						;/
-				STA $08						;>move towards $08 because 16bit*16bit multiplication uses $00 to $07
-				
-			.RoundToFullCheck
-				LDA.w #!Setting_SpriteHP_GraphicalBar_TotalPieces	;>Amount of pieces in the bar
-				LDY #$00
-				CMP $08							;>Compare with quotient (rounded)
-				BNE ..FillAmountTransferBack
-				LDY #$02						;>Rounded up fill equals maximum fill, which means it rounded up to maxpieces
-				
-				..FillAmountTransferBack
-					LDA $08
-					STA $00
-					BRA .Done
-			.NoRoundQuotient
-				LDY #$00					;>Default that the meter didn't round towards empty/full.
-				LDA $00						;\if the rounded down (result from fraction part is less than .5) quotient value ISN't zero,
-				BNE .Done					;/(exactly 1 piece filled or more) don't even consider setting Y to #$01.
-				LDA $04						;\if BOTH rounded down quotient and the remainder are zero, the bar is TRUELY completely empty
-				BEQ .Done					;/and don't set Y to #$01.
-				
-				LDY #$01					;>indicate that the value was rounded down towards empty
-				
-				.Done
-				SEP #$20
-			RTL
-	endif
-	RemoveRecordRoundDown:
-		;Output:
-		; - $00~$03: Amount of fill in the bar (rounded down)
-		; - $04~$05: Remainder ranging from 0 to MaxHeath-1 to be used for rounding
-		; - Y: Round to zero flag: $00 = nom $01 = yes.
-		LDA !Freeram_SpriteHP_CurrentHPLow,x
-		STA $00
-		LDA #$00
-		STA $01
-		REP #$20
-		LDA.w #!Setting_SpriteHP_GraphicalBar_TotalPieces
-		STA $02
-		SEP #$20
-		JSL MathMul16_16				;>CurrentHP * TotalPiecesInBar...
-		REP #$20
-		LDA $04
-		STA $00
-		LDA $06
-		STA $02
-		LDA !Freeram_SpriteHP_MaxHPLow,x
-		STA $04
-		if !Setting_SpriteHP_TwoByte
-			LDA !Freeram_SpriteHP_MaxHPHi,x
-			STA $05
-		else
-			STZ $05
-		endif
-		SEP #$20
-		JSL MathDiv32_16				;>Divided by Max HP. Quotient (at the moment): Fill amount (rounded down), Remainder: A value from 0 to MaxHeath-1 to determine a rounding operation
-		REP #$20
-		LDY #$00
-		LDA $00
-		ORA $02
-		BNE .NotRoundedToZero
-		LDA $04
-		BEQ .NotRoundedToZero
-		
-		.RoundedToZero
-			LDY #$01
-		.NotRoundedToZero
-		SEP #$20
-		RTL
-	if !Setting_SpriteHP_BarFillRoundDirection == 2
-		RemoveRecordRoundUP
-			JSL RemoveRecordRoundDown
-			REP #$20
-			LDA $04				;\If remainder is zero, (meaning exactly an integer), don't increment
-			BEQ .NoRoundUp			;/
-			.RoundUp
-				LDY #$00
-				INC $00							;>Otherwise if there is a remainder (between Quotient and Quotient+1), use Quotient+1
-				LDA.w #!Setting_SpriteHP_GraphicalBar_TotalPieces
-				CMP $00
-				BNE .NoRoundToMax					;>If fill at max, after rounding, flag it as rounded to max.
-				LDY #$02
-			.NoRoundToMax
-			.NoRoundUp
-			SEP #$20
-			RTL
-	endif
-
-	;Math routines
-		if !sa1 == 0
-			;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-			; 16bit * 16bit unsigned Multiplication
-			;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-			; Argusment
-			; $00-$01 : Multiplicand
-			; $02-$03 : Multiplier
-			; Return values
-			; $04-$07 : Product
-			;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-			MathMul16_16:	REP #$20
-					LDY $00
-					STY $4202
-					LDY $02
-					STY $4203
-					STZ $06
-					LDY $03
-					LDA $4216
-					STY $4203
-					STA $04
-					LDA $05
-					REP #$11
-					ADC $4216
-					LDY $01
-					STY $4202
-					SEP #$10
-					CLC
-					LDY $03
-					ADC $4216
-					STY $4203
-					STA $05
-					LDA $06
-					CLC
-					ADC $4216
-					STA $06
-					SEP #$20
-					RTL
-		else
-			;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-			; 16bit * 16bit unsigned Multiplication SA-1 version
-			;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-			; Argusment
-			; $00-$01 : Multiplicand
-			; $02-$03 : Multiplier
-			; Return values
-			; $04-$07 : Product
-			;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-		
-			MathMul16_16:	STZ $2250
-					REP #$20
-					LDA $00
-					STA $2251
-					ASL A
-					LDA $02
-					STA $2253
-					BCS +
-					LDA.w #$0000
-			+		BIT $02
-					BPL +
-					CLC
-					ADC $00
-			+		CLC
-					ADC $2308
-					STA $06
-					LDA $2306
-					STA $04
-					SEP #$20
-					RTL
-		endif
-		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-		; Unsigned 32bit / 16bit Division
-		; By Akaginite (ID:8691), fixed the overflow
-		; bitshift by GreenHammerBro (ID:18802)
-		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-		; Arguments
-		; $00-$03 : Dividend
-		; $04-$05 : Divisor
-		; Return values
-		; $00-$03 : Quotient
-		; $04-$05 : Remainder
-		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-	
-		MathDiv32_16:	REP #$20
-				ASL $00
-				ROL $02
-				LDY #$1F
-				LDA.w #$0000
-		-		ROL A
-				BCS +
-				CMP $04
-				BCC ++
-		+		SBC $04
-				SEC
-		++		ROL $00
-				ROL $02
-				DEY
-				BPL -
-				STA $04
-				SEP #$20
-				RTL
-	;Rounding
-		RoundAwayEmpty:
-			CPY #$01
-			BNE .No
-			.AwayFromZero
-				INC $00
-			.No
-			RTL
-		RoundAwayFull:
-			CPY #$02
-			BNE .No
-			.AwayFromFull
-				DEC $00
-			.No
-			RTL
-		RoundAwayEmptyFull:
-			CPY #$01
-			BEQ RoundAwayEmpty_AwayFromZero
-			CPY #$02
-			BEQ RoundAwayFull_AwayFromFull
-			RTL
