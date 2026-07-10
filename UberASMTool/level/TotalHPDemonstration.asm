1a6eff3010ef66eb949aad661b29ba7f6961807e
GhbSmwc <GhbSmwc@users.noreply.github.com>
2026-07-10 17:38:09 -0400

    some fixes and further tests

--- UberASMTool/level/TotalHPDemonstration.asm
+++ UberASMTool/level/TotalHPDemonstration.asm
@@ -2,14 +2,21 @@
 ;NOTE: Make sure the level using this ASM code is only 1-screen long, and should only contain and spawns
 ;sprites that cannot generate more sprites, and what it spawns are only killable sprites.
 ;
-;In this example, it simply spawns a single Chargin Chuck after 2 Rexes were killed. Assuming no options
-;were changed on their HP amounts:
-; 2 Rexs: 2HP each (4 HP for both)
-; 1 Chargin chuck: 15 HP
-; Total: 19 HP.
+;In this example, it simply spawns 2 Chargin Chucks after the inital 2 Rexes and Chargin Chuck were killed.
+;Assuming no options were changed on their HP amounts:
+;Inital enemies:
+; 2 Rexs: 2HP each (4HP for both)
+; 1 Chuck: 15 HP
+;After the inital enemies killed:
+; 2 Chargin chucks: 15HP each (30HP for both)
+;
+;The result is 49 total HP. This should be for !StartingTotalHP. For counting enemies that will spawn later
+;after the player kills, that is 30HP, which means !StartingHP_SpritesYetToLoad should be set to 30 (which
+;will set the RAM !Freeram_SpriteHP_TotalHPOfUnloadedSprites to hold a value of 30).
 
-;This is a test ASM, not meant to be used in an actual game, rather as a tutorial on how to get your
-;custom ambush system to work.
+;This is a test ASM, not meant to be used in an actual game (not well optimized, espically if you're
+;spawning lot of sprites, which adds code overhead), rather as a tutorial on how to get your custom
+;ambush system to work with this total HP system.
 
 ;Don't touch
 	incsrc "../SharedSubroutineDefs.asm"
@@ -18,23 +25,71 @@
 	incsrc "../GraphicalBarDefines.asm"
 	incsrc "../NumberDisplayRoutinesDefines.asm"
 
-	assert !Freeram_SpriteHP_TotalMaxHP == 2, "This test ASM code requires total HP system."
+	assert !Setting_SpriteHP_TotalHPMode == 2, "This test ASM code requires the total HP system."
 ;Some settings
-	!StartingTotalHP #= (!Setting_SpriteHP_VanillaSprite_Rex_HPAmount*2)+!Setting_SpriteHP_VanillaSprite_Chucks_HPAmount
+	!StartingTotalHP #= (!Setting_SpriteHP_VanillaSprite_Rex_HPAmount*2)+(!Setting_SpriteHP_VanillaSprite_Chucks_HPAmount*3)
 		;^Total HP. This includes sprite loaded, and sprite yet to load. With default settings, that should be 19.
-	!StartingHP_SpritesYetToLoad = !Setting_SpriteHP_VanillaSprite_Chucks_HPAmount
+	!StartingHP_SpritesYetToLoad = (!Setting_SpriteHP_VanillaSprite_Chucks_HPAmount*2)
 		;^Total HP of sprites that have yet to spawn into the level.
-	;Spawn XY position of a chargin chuck after all previous enemies not exists.
-		!SpriteSpawnXPos = $00E0
-		!SpriteSpawnYPos = $0170
 	
 	!RAM_HaveSpawnFlag = $1421|!addr ;>Reusing the 1-up checkpoint system, for demonstration purposes
 
+macro Spawn(SpriteNumber, IsCustom, XPos, YPos, SpawnHealth)
+	if <IsCustom> == 0
+		CLC
+	else
+		SEC
+	endif
+	LDA.b #<SpriteNumber>
+	LDX #!sprite_slots-1
+	%UberRoutine(SpawnSprite)
+	BCS ?No
+	
+	?XYPos:
+		LDA.b #<XPos>
+		STA !sprite_x_low,x
+		LDA.b #<XPos>>>8
+		STZ !sprite_x_high,x
+		LDA.b #<YPos>
+		STA !sprite_y_low,x
+		LDA.b #<YPos>>>8
+		STA !sprite_y_high,x
+	?SpawningHP:
+		;Note: Not sure if the spawn sprite subroutine does utilize
+		;SMW's $07F722-$07F78A. Just to be safe, I'll have the HP
+		;initilization here anyways.
+		LDA.b #<SpawnHealth>
+		STA !Freeram_SpriteHP_CurrentHPLow,x
+		STA !Freeram_SpriteHP_MaxHPLow,x
+		if !Setting_SpriteHP_TwoByte
+			LDA.b #<SpawnHealth>>>8
+			STA !Freeram_SpriteHP_CurrentHPHi,x
+			STA !Freeram_SpriteHP_MaxHPHi,x
+		endif
+	?DeductHPOfUnloaded:
+		;Here, every time you spawn a sprite, you need to, within that
+		;frame of spawning the sprite, deduct the value in
+		;!Freeram_SpriteHP_TotalHPOfUnloadedSprites by how much HP
+		;that spawned sprite has, else the meter fills upward because
+		;a sprite went from not being loaded, to now being loaded
+		;without "taking its health with it".
+		LDA !Freeram_SpriteHP_TotalHPOfUnloadedSprites
+		SEC
+		SBC.b #<SpawnHealth>
+		STA !Freeram_SpriteHP_TotalHPOfUnloadedSprites
+		if !Setting_SpriteHP_TwoByte
+			LDA !Freeram_SpriteHP_TotalHPOfUnloadedSprites+1
+			SBC.b #<SpawnHealth>>>8
+			STA !Freeram_SpriteHP_TotalHPOfUnloadedSprites+1
+		endif
+	?No:
+endmacro
+
 init:
-	LDA.b #!Setting_SpriteHP_VanillaSprite_Chucks_HPAmount
+	LDA.b #!StartingHP_SpritesYetToLoad
 	STA !Freeram_SpriteHP_TotalHPOfUnloadedSprites
 	if !Setting_SpriteHP_TwoByte
-		LDA.b #!Setting_SpriteHP_VanillaSprite_Chucks_HPAmount>>8
+		LDA.b #!StartingHP_SpritesYetToLoad>>8
 		STA !Freeram_SpriteHP_TotalHPOfUnloadedSprites+1
 	endif
 	LDA.b #!StartingTotalHP
@@ -71,67 +126,19 @@ main:
 	.Loop
 		LDA !14C8,x
 		BNE .ThereIsLoadedEnemy
-		
 		..Next
 			DEX
 			BPL .Loop
-	.ThereIsNoLoadedEnemy
-		..SpawnASprite
-			LDA #$91			;>Chargin chuck
-			CLC
-			LDX #!sprite_slots-1
-			%UberRoutine(SpawnSprite)
-			...SoundEffect
-				LDA #$10
-				STA $1DF9|!addr
-			...SetXYPos
-				LDA.b #!SpriteSpawnXPos
-				STA !sprite_x_low,x
-				LDA.b #!SpriteSpawnXPos>>8
-				STZ !sprite_x_high,x
-				LDA.b #!SpriteSpawnYPos
-				STA !sprite_y_low,x
-				LDA.b #!SpriteSpawnYPos>>8
-				STA !sprite_y_high,x
-			...SetItsHP
-				;Note: The spawn sprite subroutine does NOT utilize SMW's $07F722-$07F78A,
-				;thus its HP values are not initalized (garbage values).
-				LDA.b #!Setting_SpriteHP_VanillaSprite_Chucks_HPAmount
-				STA !Freeram_SpriteHP_CurrentHPLow,x
-				STA !Freeram_SpriteHP_MaxHPLow,x
-				if !Setting_SpriteHP_TwoByte
-					LDA.b #!Setting_SpriteHP_VanillaSprite_Chucks_HPAmount>>8
-					STA !Freeram_SpriteHP_CurrentHPHi,x
-					STA !Freeram_SpriteHP_MaxHPHi,x
-				endif
-			...DeductHPBecauseItIsLoaded
-				;Here, every time you spawn a sprite, you need to, within that
-				;frame of spawning the sprite, deduct the value in
-				;!Freeram_SpriteHP_TotalHPOfUnloadedSprites by how much HP
-				;that spawned sprite has, else the meter fills upward because
-				;a sprite went from not being loaded, to now being loaded
-				;without "taking its health with it". Using default
-				;settings:
-				;
-				; Rex_A: 2HP, Rex_B: 2HP, UnloadedSprites: 15 HP = 19HP
-				;
-				; After Mario defeats both rexes and a Chargin Chuck spawns:
-				;
-				; Chuck: 15HP, UnloadedSprites: 0 HP = 15HP
-				;
-				LDA !Freeram_SpriteHP_TotalHPOfUnloadedSprites
-				SEC
-				SBC.b #!Setting_SpriteHP_VanillaSprite_Chucks_HPAmount
-				STA !Freeram_SpriteHP_TotalHPOfUnloadedSprites
-				if !Setting_SpriteHP_TwoByte
-					LDA !Freeram_SpriteHP_TotalHPOfUnloadedSprites+1
-					SBC.b #!Setting_SpriteHP_VanillaSprite_Chucks_HPAmount>>8
-					STA !Freeram_SpriteHP_TotalHPOfUnloadedSprites+1
-				endif
-		..MarkedAsItSpawned
-			INC !RAM_HaveSpawnFlag
-		BRA .Done
+	.NoEnemiesExisted
+		JSR HandleSpawning
 	.ThereIsLoadedEnemy
 	.Done
 		PLB
 		RTL
+HandleSpawning:
+	%Spawn($91, 0, $00E0, $0170, !Setting_SpriteHP_VanillaSprite_Chucks_HPAmount)
+	%Spawn($91, 0, $0010, $0170, !Setting_SpriteHP_VanillaSprite_Chucks_HPAmount)
+	LDA #$10
+	STA $1DF9|!addr
+	INC !RAM_HaveSpawnFlag
+	RTS
